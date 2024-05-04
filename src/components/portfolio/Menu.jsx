90e9118322f45767e385d757dbfafa8db8a57aa2
Indula-Perera <indulaperera.lk@hotmail.com>
2024-05-04 12:32:29 +0530

    Commit

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -6,18 +6,28 @@ import Work5 from "../../assets/ZAraW.png";
 import Work6 from "../../assets/TravalWeb.png";
 import Work7 from "../../assets/POS.png";
 import Work8 from "../../assets/jss.png";
+import Work9 from "../../assets/log.png";
 
 const Menu = [
 	{
 		id: 1,
+		image: Work9,
+		title: "React Login",
+		category: ["React","Frontend"],
+		//discription: " Fulstrack Mearn Bolg App",
+		url: "#",
+		repositoryUrl: "https://github.com/Indula-Perera/Login-Register-React"
+	},
+	{
+		id: 2,
 		image: Work1,
-		title: "React Blog App",
-		category: ["MERN", "React"],
+		title: "Next.js Blog App",
+		category: ["Next.js", "React"],
 		//discription: " Fulstrack Mearn Bolg App",
 		repositoryUrl: "https://github.com/Indula-Perera/React-Blog-App.git"
 	},
 	{
-		id: 2,
+		id: 3,
 		image: Work2,
 		title: "React Portfolio",
 		category: ["React"],
@@ -26,7 +36,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/React-Portfolio.git"
 	},
 	{
-		id: 3,
+		id: 4,
 		image: Work3,
 		title: "Admin Dashboard",
 		category: ["Frontend", "React"],
@@ -35,7 +45,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/React-Admin-Dashboard.git"
 	},
 	{
-		id: 4,
+		id: 5,
 		image: Work4,
 		title: "OpenAi Articles Summarizer",
 		category: ["React"],
@@ -44,7 +54,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/OpenAI-Article-Summarizer.git"
 	},
 	{
-		id: 5,
+		id: 6,
 		image: Work5,
 		title: "Zara Wide Clothing Store",
 		category: ["PHP"],
@@ -53,7 +63,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/Full-Functional-PHP-e-commerce-website-HD-Final-Project.git"
 	},
 	{
-		id: 6,
+		id: 7,
 		image: Work6,
 		title: "Trip Planning And Booking Web App",
 		category: ["PHP"],
@@ -62,7 +72,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/Travel-Website--Road-Trip-Srilanka.git"
 	},
 	{
-		id: 7,
+		id: 8,
 		image: Work7,
 		title: "E Commerce Shop POS System",
 		category: ["PHP"],
@@ -71,7 +81,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/Indula-Perera/Point-of-Sale-Project.git"
 	},
 	{
-		id: 8,
+		id: 9,
 		image: Work8,
 		title: "Image Search Web App",
 		category: ["JavaScript"],
@@ -79,15 +89,7 @@ const Menu = [
 		url: "#",
 		repositoryUrl: "https://github.com/Indula-Perera/JS-Image-Search.git"
 	},
-	{
-		id: 9,
-		image: Work8,
-		title: "React Login",
-		category: ["React"],
-		//discription: " Fulstrack Mearn Bolg App",
-		url: "#",
-		repositoryUrl: "https://github.com/Indula-Perera/JS-Image-Search.git"
-	}
+
 ];
 
 export default Menu;
